36a7f87e9525f65d93ad2c425ad5011fd190f03c
JackTPatterson <Jtpatt03@gmail.com>
2025-04-12 17:34:55 -0400

    commit

---
+++ src/app/results/page.tsx
@@ -0,0 +1,19 @@
+import {LoginForm} from "@/app/(auth)/login/LoginPage";
+import {KeyboardIcon} from "lucide-react";
+import React from "react";
+import type {Metadata} from "next";
+import {TypingAnimation} from "@/components/ui/typing-animation";
+import {ResultsPage} from "@/app/results/ResultsPage";
+import {Header} from "@/components/templates/Header";
+
+export const metadata: Metadata = {
+    title: "Results",
+};
+
+
+export default function Login(){
+    return <div className={'h-screen'}>
+        <Header/>
+        <ResultsPage/>
+    </div>
+}
